eddeef80bbc96c7a5f74fc02ebc0eb0fe8ace72a
unicorn-glitter-sprinkles <141748606+unicorn-glitter-sprinkles@users.noreply.github.com>
2025-04-08 09:50:16 -0500

    r

--- datatypes.php
+++ datatypes.php
@@ -7,17 +7,27 @@
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
   </head>
   <body>
-  
-  <form method= "GET">
-    <input type= "text" name= "person">
-    <button>SUBMIT</button>
 
   <?php
-  //This gets something from thr URL and passes into the code
-    $name = $_GET ['person'];
-    //This takes the name and puts it into the sentence
-    echo $name. "is a handsome fellow!";
+ 
+        //String
+        $name = 'Coding is fun';
+
+        //Integer
+        $name = 20;
+
+        //Float
+        $name = 20.5784;
+
+        //Boolean
+        true = 1
+        false = 0
+
+        //Array
+        $names = array("Daniel","Dennis","Michael");
+        echo $names ['0'];
   ?>
+
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
   </body>
 </html>
